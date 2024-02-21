875044aa240c4e0c318db63d894c115720918672
aldanamedina <aldana.medina@udc.es>
2024-02-21 01:27:49 +0100

    métodos y docstrings
    añadi todos los metodos que pide (revisar).
    
    tambien empece la estructura de los docstrings en todas las funciones y clases que hay.
    
    puse Así los metodos ya que estan de esa manera en la practica:
        def basic_attack(opponent:'Pokemon') -> int:
    
    Ayuda: no se que es 'p' en select next pokemon en trainer.py

--- pokemon.py
+++ pokemon.py
@@ -9,12 +9,12 @@ class Pokemon(ABC):
         Parameters:
             name (str):
             level (int)
-            strenght(int):
-            defense(int)
-            hp(int)
-            total_hp(int): 
-            agility(int):
-            pokemon_type(str):
+            strenght (int):
+            defense (int)
+            hp (int)
+            total_hp (int): 
+            agility (int):
+            pokemon_type (str):
         """      
         self.name = name
         self.level = level
@@ -25,42 +25,178 @@ class Pokemon(ABC):
         self.agility = agility
         self.pokemon_type = pokemon_type
 
-    def basic_attack(p):
+    def basic_attack(opponent:'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
         pass
 
-    def is_debilitated():
+    def is_debilitated() -> bool:
+        """
+        Descripcion.
+        
+        Returns:
+            bool:
+        """
         pass
 
     @abstractmethod
-    def effectiveness(p):
-         pass  
-    
+    def effectiviness(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        pass  
+    def __str__(self) -> str:
+        """ y redefinirá el método mágico __str__() para que al mostrar por pantalla una instancia de un objeto Pokemon 
+        devuelva un string con información en el formato:  
+                {name} ({pokemon_type}) Stats: Level: {level}, ATT: {strength}, DEF: {defense}, AGI: {agility}, HP: {hp/total_hp}. 
+                donde los valores entre {} indican los valores de los atributos del Pokémon.
+
+        Bulbasaur (Grass) Stats: Level: 55, ATT: 67, DEF: 29, AGI: 32, HP: 45/55 
+        (donde el 45/55 refleja que el Pokemon ha sufrido 10 puntos de daño respecto a sus puntos de vida totales)."""
+
+        info = '{name} ({pokemon_type} Stats: )'.format(self.get_name(), self.get_pokemon_type())
+        info += 'Level: {level}, ATT: {strength}, DEF: {defense}, AGI: {agility}, HP: {hp/total_hp}.'.format(self.get_level(), self.get_strength(), self.get_defense(), self.get_agility())
+        return info #COMO SE PONE EL HP?? 
 
     
 
 class WaterPokemon(Pokemon):
     def __init__(self, name, level, strenght, defense, hp, total_hp, agility, pokemon_type, surge_mode):
-            """Inicializa.
-            Parameters:
-                surge_mode(bool):   
-            """
-            self.surge_mode = False
-            super().__init__(name, level, strenght, defense, hp, total_hp, agility, pokemon_type)
-            
+        """
+        Inicializa.
+        
+        Parameters:
+            surge_mode(bool):   
+        """
+        self.surge_mode = False
+        super().__init__(name, level, strenght, defense, hp, total_hp, agility, pokemon_type)
+    def water_attack(opponent:'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        pass
+    def check_surge_activation() -> bool:
+        """
+        Descripcion.
+        
+        Returns:
+            bool:
+        
+        """
+        pass
+    def effectiveness(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        return super().effectiveness() # lo de super me sale directamente ig pq ya esta en la clase Pokemon
 class FirePokemon(Pokemon):
     def __init__(self, name, level, strenght, defense, hp, total_hp, agility, pokemon_type, temperature):
-            """Inicializa.
-            Parameters:
-                temperature(float):
-            """
-            self.temperature = temperature
-            super().__init__(self, name, level, strenght, defense, hp, total_hp, agility, pokemon_type)
+        """
+        Inicializa.
+        
+        Parameters:
+            temperature(float):
+        """
+        self.temperature = temperature
+        super().__init__(self, name, level, strenght, defense, hp, total_hp, agility, pokemon_type)
+    def fire_attack(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
 
+        Returns:
+            int:
+        """
+        pass
+    def embers(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        pass
+    def effectiveness(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        return super().effectiveness()
 class GrassPokemon(Pokemon):
     def __init__(self, name, level, strenght, defense, hp, total_hp, agility, pokemon_type, healing):
         """Inicializa.
+        
         Parameters:
-            healing(float):
+            healing (float):
         """
         self.healing = healing
         super().__init__(name, level, strenght, defense, hp, total_hp, agility, pokemon_type)
+    
+    def grass_attack(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        pass
+    def heal(): 
+        """
+        Descripcion.
+        
+        Parameters: 
+            None.
+        
+        Returns:
+            int:
+        """
+        pass
+    def effectiveness(opponent: 'Pokemon') -> int:
+        """
+        Descripcion.
+        
+        Parameters:
+            opponent ():
+
+        Returns:
+            int:
+        """
+        return super().effectiveness()
--- trainer.py
+++ trainer.py
@@ -2,17 +2,44 @@
 TODO: Implement in the file the Trainer class
 """
 from pokemon import *
-class Trainer:
+class Trainer: 
     def __init__(self, name, pokemon):
+        """
+        Descripcion.
+
+        Parameters:
+            name (str):
+            pokemon (list):
+        """
         self.name = name
         self.pokemon = pokemon
     def select_initial_pokemon():
+        """
+        Descripcion.
+
+        Parameters:
+            None.
+
+        Returns:
+        """
         pass
     def select_next_pokemon(p: Pokemon):
-        pass
-    def all_debilitated()
         """
+        Descripcion.
+        
+        Parameters:
+            p (list):???
+        
         Returns:
-            aaaa(bool)
-            
+        """
+        pass
+    def all_debilitated():
+        """
+        Descripcion
+        
+        Parameters:
+            None.
+        
+        Returns:
+            bool:
         """
