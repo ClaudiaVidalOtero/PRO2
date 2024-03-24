1af3cebe35b5fb778dd10c63a6cd3f73698d3111
ClaudiaVidalOtero <claudia.votero@udc.es>
2024-03-24 11:36:40 +0100

    commit

--- queue_handler.py
+++ queue_handler.py
@@ -5,6 +5,7 @@ Claudia Vidal Otero (claudia.votero@udc.es)
 from array_queue import ArrayQueue
 from process import Process
 class Usuario:
+
     def __init__(self, id_usuario, penalizacion):
         self.id_usuario = id_usuario
         self.penalizacion = penalizacion
@@ -21,9 +22,10 @@ class Usuario:
         return f"Usuario({self.id_usuario}, {self.penalizacion})"
 
 class ProcessManager:
-    def __init__(self, cola_registro: ArrayQueue[Process], time):
+    def __init__(self, cola_registro: ArrayQueue[Process], cola_ejecucion):
 
         self._cola_registro = cola_registro
+        self._cola_ejecucion = cola_ejecucion
         
 
     def elegir_colas(self, cola_registro):
@@ -54,13 +56,14 @@ class ProcessManager:
             else:
                 print("supererror slaynt sach")
                 
-    def ejecucion(self, cola_gpu_short,cola_gpu_long, cola_cpu_short,cola_cpu_long):
+    def ejecucion(self, cola_registro, cola_ejecucion, cola_gpu_short, cola_gpu_long, cola_cpu_short, cola_cpu_long):
         cola_ejecucion = ArrayQueue()
         time = 0 #eso mejor bucle akdana
-        for proceso in cola_gpu_short:
-            pass
+        while not cola_ejecucion.is_empty() and not (cola_gpu_short.is_empty() or cola_gpu_long.is_empty() or cola_cpu_short.is_empty() or cola_cpu_long.is_empty()) and not cola_registro.is_empty()
+            time+=1
+            
 
-    def agregar_proceso_ejecucion(self, proceso_a_añadir,cola_ejecucion,time):
+    def agregar_proceso_ejecucion(self, proceso_a_añadir, cola_ejecucion,time):
         """Comprueba que no haya ningún proceso de su misma cola, y si no hay, o este ya cumplió su tiempo añade el nuevo"""
 
         subcola = f"cola_{proceso_a_añadir.resource_type}_{proceso_a_añadir.execution_time}"
@@ -71,30 +74,19 @@ class ProcessManager:
             subcola2 = f"cola_{proceso.resource_type}_{proceso.execution_time}"
 
 
-            if subcola == subcola2 and (time - proceso.start_time) == proceso.execution_time: 
+            if (subcola == subcola2) and ((time - proceso.start_time) == proceso.execution_time): 
                 cola_ejecucion.dequeue(proceso)
+                print(f"El proceso existente de la sunbcola {subcola} ha finalizado su ejecución")
                 cola_ejecucion.enqueue(proceso_a_añadir)
                 print(f"Añadido un nuevo proceso de la subcola: {subcola}.")
                 
             
             else: 
                 cola_revisada.enqueue(proceso)
+
         cola_ejecucion = cola_revisada
         
         return cola_ejecucion
-        
-
-    def remover_proceso_ejecucion(self, proceso):
-        subcola = f"cola_{proceso.resource_type}_{proceso.execution_time}"
-        if subcola in self.cola_procesos_en_ejecucion and self.cola_procesos_en_ejecucion[subcola] == proceso:
-            del self.cola_procesos_en_ejecucion[subcola]
-            return True
-        else:
-            print(f"El proceso {proceso} no está en ejecución.")
-            return False
-             
-
-
 
     def usuario_penalizado(self,id_usuario):
         usuario_en_cola = None
@@ -107,7 +99,8 @@ class ProcessManager:
                 return False
 
     def añadir_usuario_cola(self, id_usuario):
-        """Revisa si un usuario está en la cola de penalizados, si ya está, le añade 5 ud de tiempo a la penalización, si no simplemente lo añade con penalización 5
+        """
+        Revisa si un usuario está en la cola de penalizados, si ya está, le añade 5 ud de tiempo a la penalización, si no simplemente lo añade con penalización 5
         """
         usuario_en_cola = None
 
@@ -124,7 +117,9 @@ class ProcessManager:
             self.cola_usuarios_penalizados.append(nuevo_usuario)
 
     def control_penalizacion_usuarios(self, cola_usuarios_penalizados):
-        """Revisa todos los usuarios y elimina aquellos que ya cumplieron su penalizacióm, para los demás les resta una ud de tiempo a su penalizacion"""
+        """
+        Revisa todos los usuarios y elimina aquellos que ya cumplieron su penalizacióm, para los demás les resta una ud de tiempo a su penalizacion.
+        """
 
         cola_penalizados_revisada = ArrayQueue()
 
@@ -142,59 +137,4 @@ class ProcessManager:
 
         return cola_usuarios_penalizados 
 
-
-
-
-
-
-
-class GestorColas:
-    def __init__(self):
-        self.cpu_short = ColaEjecucion()
-        self.cpu_long = ColaEjecucion()
-        self.gpu_short = ColaEjecucion()
-        self.gpu_long = ColaEjecucion()
-        self.procesos_en_ejecucion = {}
-
-    def agregar_proceso(self, proceso):
-        if proceso.tipo == 'cpu':
-            if proceso.tiempo_estimado == 'short':
-                self.cpu_short.agregar_proceso(proceso)
-            else:
-                self.cpu_long.agregar_proceso(proceso)
-        else:
-            if proceso.tiempo_estimado == 'short':
-                self.gpu_short.agregar_proceso(proceso)
-            else:
-                self.gpu_long.agregar_proceso(proceso)
-
-    def ejecutar_procesos(self, tiempo_actual):
-        procesos_terminados = []
-        for tipo_cola in [self.cpu_short, self.cpu_long, self.gpu_short, self.gpu_long]:
-            if tipo_cola.obtener_proceso() is not None:
-                proceso = tipo_cola.obtener_proceso()
-                if proceso.id_usuario in self.procesos_en_ejecucion:
-                    if tiempo_actual >= self.procesos_en_ejecucion[proceso.id_usuario]['t_inicio_ejecucion'] + proceso.tiempo_ejecucion:
-                        procesos_terminados.append(proceso)
-                        del self.procesos_en_ejecucion[proceso.id_usuario]
-                else:
-                    proceso.tiempo_inicio_ejecucion = tiempo_actual
-                    self.procesos_en_ejecucion[proceso.id_usuario] = {'t_inicio_ejecucion': tiempo_actual, 'tipo_cola': tipo_cola}
-
-        for proceso in procesos_terminados:
-            print(f"Proceso terminado: {tiempo_actual} {proceso.id_proceso} {proceso.id_usuario} {proceso.tipo} {proceso.tiempo_estimado} {proceso.tiempo_entrada} {proceso.tiempo_inicio_ejecucion} {proceso.tiempo_ejecucion}")
-            if proceso.tiempo_estimado == 'short' and proceso.tiempo_ejecucion > 5:
-                print(f"Penalización aplicada: {tiempo_actual} {proceso.id_proceso} {proceso.id_usuario}")
-            del proceso
-
-    def penalizar_usuario(self, tiempo_actual, id_usuario):
-        cola_usuarios_penalizados = ArrayQueue()
-
-        if id_usuario is not cola_usuarios_penalizados():         
-            cola_usuarios_penalizados.enqueue([tiempo_actual,id_usuario])
-        else:
-            cola_usuarios_penalizados.dequeue(el usuario )
-            cola_usuarios_penalizados.enqueue([tiempo_actual,id_usuario])
-        
-
-        print(f"Penalización activa: {tiempo_actual} {id_usuario}")
+ 
